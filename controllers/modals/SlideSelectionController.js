11572f2e4267a0f931e1a751e87eaa7114d7f8b4
Ron Gilchrist <kid@thewebkid.com>
2016-08-27 19:06:52 -0700

    added slide overlays list, selection logic, slide numbers, and new menu items

---
+++ controllers/modals/SlideSelectionController.js
@@ -0,0 +1,30 @@
+﻿wwt.controllers.controller('SlideSelectionController', ['$scope', function ($scope) {
+    var tourScope = angular.element('#currentTourPanel').scope();
+    var overlays, selectionSet, selection;
+    var init = function () {
+        selection = tourScope.tourEdit.tourEditorUI.selection;
+        selectionSet = $scope.selectionSet = selection.selectionSet;
+        
+        overlays = $scope.overlays = tourScope.selectedSlide._overlays;
+        $scope.$applyAsync(function () {
+            overlays.forEach(function (overlay, j) {
+                overlay.selected = selection.isOverlaySelected(overlay);
+            });
+        });
+    }
+
+    $scope.selectionChange = function (overlay) {
+        selection.clearSelection();
+        var range = [];
+        overlays.forEach(function (overlay, i) {
+            if (overlay.selected) {
+                range.push(overlay);
+            }
+        });
+        selection.addSelectionRange(range);
+
+    }
+
+    $('canvas').on('dblclick click', init);
+    tourScope.$on('initSlides', init);
+}]);
