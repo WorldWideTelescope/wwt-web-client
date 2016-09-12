52c24bd42de0f47c7b8eb6c683c8722e28a8e7b9
Ron Gilchrist <kid@thewebkid.com>
2016-09-11 23:22:40 -0700

    added safari test in edit mode.

--- controllers/tabs/CurrentTourController.js
+++ controllers/tabs/CurrentTourController.js
@@ -68,6 +68,10 @@
     };
    
     var initEditMode = function () {
+        
+        if (navigator.userAgent.toLowerCase().indexOf('safari')>0) {
+            alert('Editing Tours has known issues in Safari. For the best experience, please use Chrome.')
+        }
         if ($rootScope.editingTour !== true) { return; }
         tour._editMode = true;
         tourEdit.pauseTour();
