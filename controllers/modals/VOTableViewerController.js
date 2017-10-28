022d2274ef2493449b923d284e08dcec345c04d7
Ron Gilchrist <kid@thewebkid.com>
2017-10-28 16:16:30 -0700

    fixed bindings to RA/DEC columns. added goto. Need to refine this more

--- controllers/modals/VOTableViewerController.js
+++ controllers/modals/VOTableViewerController.js
@@ -5,21 +5,30 @@ wwt.controllers.controller('voTableViewer',
 
         $('.wwt-modal .modal-dialog, .wwt-modal .modal-content').width(1120);
         console.log($scope.votable);
+        var colArray = [];
         if ($scope.votable && $scope.votable.columns) {
           Object.keys($scope.votable.columns).forEach(function (c, i) {
+            var col = $scope.votable.columns[c]
+            colArray.push(col);
             if (c === 'RA') {
-              $scope.RAIndex = i;
+              $scope.RASource = col.id;
+              $scope.RAIndex = col.index;
+
             }
             if (c === 'DEC') {
-              $scope.DECIndex = i;
+              $scope.DecSource = col.id;
+              $scope.DecIndex = col.index;
             }
           });
         }
+        $scope.colArray = colArray;
       }
 
 
       $scope.hilite = function (row, $index) {
-        console.log('hilite',row,$index);
+        var ra = parseFloat(row.columnData[$scope.RAIndex]);
+        var dec = parseFloat(row.columnData[$scope.DecIndex]);
+        wwt.wc.gotoRaDecZoom(ra,dec,60, true);
         $scope.hiliteIndex = $index;
       };
 
