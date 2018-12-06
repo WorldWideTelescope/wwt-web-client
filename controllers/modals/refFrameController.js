51ec9a7e014182ca4bef3820eb9b474a08332592
Ron Gilchrist <kid@thewebkid.com>
2018-12-05 18:41:45 -0800

    more reference wiz wip

--- controllers/modals/refFrameController.js
+++ controllers/modals/refFrameController.js
@@ -1,6 +1,7 @@
 wwt.controllers.controller('refFrameController', ['$scope', function ($scope) {
 
   $scope.page = 'welcome';
+  $scope.pages = ['welcome','options','position','trajectory'];
   $scope.offsetTypes = [{
     type:'FixedSherical',
     label:'Fixed Spherical'
@@ -20,6 +21,25 @@ wwt.controllers.controller('refFrameController', ['$scope', function ($scope) {
     back:false,
     finish:false
   };
-
+  var calcButtonState = function(){
+    var i = $scope.pages.indexOf($scope.page);
+    $scope.buttonsEnabled.next = $scope.page=='options';
+    $scope.buttonsEnabled.back = i >0;
+    $scope.buttonsEnabled.finish = i===$scope.pages.length-1;
+  };
+  $scope.next = function(){
+    var i = $scope.pages.indexOf($scope.page);
+    $scope.page = $scope.pages[i+1];
+    calcButtonState();
+  };
+  $scope.back = function(){
+    var i = $scope.pages.indexOf($scope.page);
+    $scope.page = $scope.pages[i-1];
+    calcButtonState();
+  };
+  $scope.finish = function(){
+    alert('tba');
+  };
   $scope.refFrameName = '';
+  calcButtonState();
 }]);
