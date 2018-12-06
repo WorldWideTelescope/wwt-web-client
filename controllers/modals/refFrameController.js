e6854cc0f92c590b946ba3be1e3aaadf7ca3ce05
Ron Gilchrist <kid@thewebkid.com>
2018-12-06 00:31:02 -0800

    databound ref frame wiz options. databound color to browser color picker. added conversion from hex->argb/argb->hex

--- controllers/modals/refFrameController.js
+++ controllers/modals/refFrameController.js
@@ -1,43 +1,51 @@
 wwt.controllers.controller('refFrameController', ['$scope', function ($scope) {
 
   $scope.page = 'welcome';
-  $scope.pages = ['welcome','options','position','trajectory'];
+  $scope.pages = ['welcome', 'options', 'position', 'trajectory'];
   $scope.offsetTypes = [{
-    type:'FixedSherical',
-    label:'Fixed Spherical'
-  },{
+    type: 'FixedSherical',
+    label: 'Fixed Spherical'
+  }, {
     type: 'Orbital',
-    label:'Orbital'
-  },{
+    label: 'Orbital'
+  }, {
     type: 'Trajectory',
-    label:'Trajectory'
-  },{
+    label: 'Trajectory'
+  }, {
     type: 'Synodic',
-    label:'Synodic'
+    label: 'Synodic'
   }];
-  $scope.offsetType='FixedSherical';
+  $scope.offsetType = 'FixedSherical';
   $scope.buttonsEnabled = {
-    next:false,
-    back:false,
-    finish:false
+    next: false,
+    back: false,
+    finish: false
   };
-  var calcButtonState = function(){
-    var i = $scope.pages.indexOf($scope.page);
-    $scope.buttonsEnabled.next = $scope.page=='options';
-    $scope.buttonsEnabled.back = i >0;
-    $scope.buttonsEnabled.finish = i===$scope.pages.length-1;
+  $scope.hexColor = '#ffffff';
+  $scope.colorChange = function(){
+    var hex = $scope.hexColor;
+    var rgb = hex.match(/[A-Za-z0-9]{2}/g).map(function(v){return parseInt(v, 16)});
+    $scope.refFrame.representativeColor.r = rgb[0];
+    $scope.refFrame.representativeColor.g = rgb[1];
+    $scope.refFrame.representativeColor.b = rgb[2];
   };
-  $scope.next = function(){
+  var calcButtonState = function () {
     var i = $scope.pages.indexOf($scope.page);
-    $scope.page = $scope.pages[i+1];
+    $scope.buttonsEnabled.next = $scope.page === 'options';
+    $scope.buttonsEnabled.back = i > 0;
+    $scope.buttonsEnabled.finish = i === $scope.pages.length - 1;
+  };
+  $scope.next = function () {
+    var i = $scope.pages.indexOf($scope.page);
+    $scope.page = $scope.pages[i + 1];
     calcButtonState();
   };
-  $scope.back = function(){
+  $scope.back = function () {
     var i = $scope.pages.indexOf($scope.page);
-    $scope.page = $scope.pages[i-1];
+    $scope.page = $scope.pages[i - 1];
     calcButtonState();
   };
-  $scope.finish = function(){
+  $scope.finish = function () {
     alert('tba');
   };
   $scope.refFrameName = '';
