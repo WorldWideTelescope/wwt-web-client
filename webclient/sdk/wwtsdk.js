7f270bb250d921af3296f59482e8ad5133f7ff24
Ron Gilchrist <kid@thewebkid.com>
2019-02-21 21:06:28 -0800

    build

--- webclient/sdk/wwtsdk.js
+++ webclient/sdk/wwtsdk.js
@@ -1602,7 +1602,8 @@ window.wwtlib = function(){
     lightYears: 7, 
     parsecs: 8, 
     megaParsecs: 9, 
-    custom: 10
+    custom: 10, 
+    kiloParsecs: 11
   };
 
 
@@ -8035,6 +8036,7 @@ window.wwtlib = function(){
       this._colors.length = 0;
       this._points.length = 0;
       this._dates.length = 0;
+      this._sizes.length = 0;
       this._emptyPointBuffer();
     },
     _emptyPointBuffer: function() {
@@ -38684,6 +38686,7 @@ window.wwtlib = function(){
       this.loadFromString(ss.safeCast(data, String), true, purgeOld, purgeAll, hasHeader);
       this.computeDateDomainRange(-1, -1);
       this._dataDirty$1 = true;
+      this.dirty = true;
       return true;
     },
     loadData: function(tourDoc, filename) {
@@ -39887,7 +39890,6 @@ window.wwtlib = function(){
           default:
             break;
         }
-        this.pointList.draw(renderContext, opacity * this.get_opacity(), false);
       }
       if (this.lineList != null) {
         this.lineList.sky = this.get_astronomical();
