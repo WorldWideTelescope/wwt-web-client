f59665dd71c9f7b4bc35d13730daeffd85781a29
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-07-24 16:12:15 +0100

    Added initial simple colormap implementation

--- HTML5SDK/wwtlib/Layers/ColorMap.cs
+++ HTML5SDK/wwtlib/Layers/ColorMap.cs
@@ -9,9 +9,36 @@ using System.Html.Media.Graphics;
 namespace wwtlib
 {
 
-    public class LinearColorMap
+    class ColorMap
     {
+        public List<Color> colors = new List<Color>();
+        public static ColorMap FromNestedLists(List<List<float>> color_list)
+        {
+            ColorMap temp = new ColorMap();
+            foreach (var color in color_list)
+            {
+                temp.colors.Add(Color.FromArgb(color[3], color[0], color[1], color[2]));
+            }
+            return temp;
+        }
+
+        public Color FindClosestColor(float value)
+        {
+            // Given a floating-point value in the range 0 to 1, find the color that is the
+            // closest to it.
+
+            int index;
+
+            if (value <= 0) {
+                return colors[0];
+            } else if (value >= 1) {
+                return colors[colors.Count - 1];
+            } else {
+                index = (int)(value * colors.Count);
+                return colors[index];
+            }
+
+        }
 
     }
-
 }
