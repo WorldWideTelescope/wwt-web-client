13dada18ff1186b259ce8e15db9fd9306a205079
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-07-24 17:20:46 +0100

    Remove unused code

--- HTML5SDK/wwtlib/Layers/ColorMap.cs
+++ HTML5SDK/wwtlib/Layers/ColorMap.cs
@@ -1,10 +1,5 @@
-using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Html;
-using System.Xml;
-using System.Html.Services;
-using System.Html.Media.Graphics;
+
 
 namespace wwtlib
 {
