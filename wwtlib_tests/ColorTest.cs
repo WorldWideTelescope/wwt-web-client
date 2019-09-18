b6ae7a5896f0ef90abb16bde7b5a0deae8901c08
Thomas Robitaille <thomas.robitaille@gmail.com>
2019-09-18 14:32:06 +0100

    Added minimal test suite with one test for Color

---
+++ wwtlib_tests/ColorTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using wwtlib;
+
+namespace wwtlib_tests
+{
+    [TestClass]
+    public class ColorTests
+    {
+        [TestMethod]
+        public void TestFromArgb()
+        {
+            Color color = Color.FromArgb(1, 2, 3, 4);
+            Assert.AreEqual(color.R, 2);
+            Assert.AreEqual(color.G, 3);
+            Assert.AreEqual(color.B, 4);
+            Assert.AreEqual(color.A, 1);
+        }
+    }
+}
