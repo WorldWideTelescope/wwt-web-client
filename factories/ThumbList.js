b0785970f9ef7c75326b94cfddd5fb334389e504
Peter Williams <peter@newton.cx>
2020-01-30 15:39:22 -0500

    factories/ThumbList.js: lightly reformat

--- factories/ThumbList.js
+++ factories/ThumbList.js
@@ -24,8 +24,7 @@ wwt.app.factory(
         goBack: goBack
       };
 
-      // Each controller calls init and passes in the controller
-      // scope
+      // Each controller calls init and passes in its scope
       function init(scope, name) {
         scope.pageCount = 1;
         scope.pageSize = 1;
@@ -34,12 +33,15 @@ wwt.app.factory(
         scope.preventClickBubble = function (event) {
           event.stopImmediatePropagation();
         };
+
         scope.goBack = function () {
           goBack(scope);
         };
+
         scope.goFwd = function () {
           goFwd(scope);
         };
+
         scope.showMenu = function (i) {
           if (!$('#researchMenu').length) {
             return $templateRequest('views/research-menu.html').then(function (tplContent) {
@@ -91,27 +93,31 @@ wwt.app.factory(
           scope.expandTop(scope.expanded, name);
           calcPageSize(scope, name === 'context');
         };
-        scope.dropdownClass = name === 'context' && !util.isMobile ? 'dropup menu-container' : 'dropdown menu-container';
-        scope.popupPosition = name === 'context' && !util.isMobile ? 'top' : 'bottom';
+
+        scope.dropdownClass = (name === 'context' && !util.isMobile ? 'dropup menu-container' : 'dropdown menu-container');
+        scope.popupPosition = (name === 'context' && !util.isMobile ? 'top' : 'bottom');
       }
 
       function clickThumb(item, scope, outParams, callback) {
-        //console.log(item)
-        setTimeout(function(){
+        setTimeout(function() {
           $rootScope.instant = false;
-        },2222);
-        if (!item){
+        }, 2222);
+
+        if (!item) {
           return console.warn(item);
         }
+
         if (item.contextMenuEvent) {
           return outParams;
         }
+
         if (!outParams) {
           outParams = {};
         }
+
         scope.activeItem = item.get_thumbnailUrl() + item.get_name();
         scope.setActiveItem(item);
-        //wwt.wc.clearAnnotations();
+
         if (item.get_name() === 'Up Level') {
           $('body').append($('#researchMenu'));
           scope.currentPage = 0;
@@ -123,10 +129,12 @@ wwt.app.factory(
           calcPageSize(scope, false);
           return outParams;
         }
+
         if (item.get_url && item.get_url() && item.get_url().indexOf('?wwtfull') !== -1) {
           window.open(item.get_url());
           return outParams;
         }
+
         if (item.get_isFolder()) {
           $('#folderLoadingModal').modal('show');
           $('body').append($('#researchMenu'));
@@ -134,23 +142,28 @@ wwt.app.factory(
           outParams.depth++;
           outParams.breadCrumb.push(item.get_name());
           scope.breadCrumb = outParams.breadCrumb;
+
           places.getChildren(item).then(function (result) {
             $('#folderLoadingModal').modal('hide');
             if ($.isArray(result[0])) {
               result = result[0];
             }
+
             var unique = [];
             $.each(result, function (index, el) {
               if ($.inArray(el, unique) === -1) unique.push(el);
             });
+
             scope.collection = unique;
-            console.log({unique:unique});
             calcPageSize(scope, false);
             outParams.cache.push(result);
+
             if (outParams.openCollection) {
               if (outParams.newCollectionUrl) {
                 var i = 0;
-                while (result[i] && result[i].url && result[i].url.indexOf(outParams.newCollectionUrl) === -1) i++;
+
+                while (result[i] && result[i].url && result[i].url.indexOf(outParams.newCollectionUrl) === -1)
+                  i++;
 
                 scope.clickThumb(result[i] || result[i - 1]);
                 outParams.newCollectionUrl = null;
@@ -163,6 +176,7 @@ wwt.app.factory(
               callback();
             }
           });
+
           return outParams;
         } else if (outParams.openCollection) {
           outParams.openCollection = false;
@@ -172,87 +186,54 @@ wwt.app.factory(
         } else if (util.isMobile) {
           $('#explorerModal').modal('hide');
         }
-        setTimeout(function(){
-          $rootScope.$applyAsync(function(){
+
+        setTimeout(function () {
+          $rootScope.$applyAsync(function () {
             $rootScope.setLookAt(null);
-          })},111);
+          })
+        }, 111);
+
         if ((item.isFGImage && item.imageSet && scope.lookAt !== 'Sky') || item.isSurvey) {
           if (item.guid && item.guid.toLowerCase().indexOf('mars.') == -1) {
             scope.setLookAt('Sky', item.get_name(), true, item.isSurvey);
           }
+
           if (item.isSurvey) {
             scope.setSurveyBg(item.get_name(), item);
           } else {
             scope.setForegroundImage(item);
           }
+
           if (scope.$hide) {
             scope.$hide();
             $rootScope.searchModal = false;
           }
+
           return outParams;
-        }
-        else if (item.isPanorama) {
-          //scope.setLookAt('Panorama', item.get_name());
+        } else if (item.isPanorama) {
           scope.setBackgroundImage(item);
         } else if (item.isEarth) {
           scope.setLookAt('Earth', item.get_name());
         } else if (util.getIsPlanet(item)) {
-
           if (scope.lookAt === 'Sky') {
-            //var c = item.get_camParams();
-            //c.zoom = 0.1;
-
             wwtlib.WWTControl.singleton.gotoTarget3(item.get_camParams());
             return outParams;
-
           }
-          if (scope.lookAt !== 'SolarSystem') {
 
+          if (scope.lookAt !== 'SolarSystem') {
             scope.setLookAt('Planet', item._name || '');
           }
         }
+
         if ((ss.canCast(item, wwtlib.Place) || item.isEarth) && !item.isSurvey) {
           scope.setForegroundImage(item);
         }
+
         if (ss.canCast(item, wwtlib.Tour)) {
           scope.playTour(item.get_tourUrl());
         }
-        return outParams;
 
-        /*var exUranus = {
-          "_camParams": {
-          "lat": -4.426167041642662,
-          "lng": -351.77882519031607,
-          "zoom": 0.1,
-          "rotation": 0,
-          "angle": 0,
-          "raDec": true,
-          "opacity": 100,
-          "target": 65536,
-          "viewTarget": {"x": 0, "y": 0, "z": 0},
-          "targetReferenceFrame": ""
-          },
-          "_location3d": {"x": 0, "y": 0, "z": 0},
-          "htmlDescription": "",
-          "_constellation": "AQR",
-          "_classification": 536870912,
-          "_type": 2,
-          "_magnitude": 0,
-          "_distnace": 0,
-          "angularSize": 0.1,
-          "_thumbNail": null,
-          "_studyImageset": null,
-          "_backgroundImageSet": null,
-          "_searchDistance": 0,
-          "_elevation": 50,
-          "_name": "Uranus",
-          "guid": "Solar System (Sky).8",
-          "thumb": "//cdn.worldwidetelescope.org/wwtweb/thumbnail.aspx?name=uranus",
-          "isPlanet": false,
-          "isFGImage": true,
-          "$$hashKey": "object:832",
-          "imageSet": null
-          }*/
+        return outParams;
       };
 
       function calcPageSize(scope, isContextPanel) {
@@ -263,12 +244,15 @@ wwt.app.factory(
         if (isContextPanel && (scope.lookAt === 'Sky' || scope.lookAt === 'SolarSystem')) {
           winWid = winWid - 216; //angular.element('body.desktop .fov-panel').width();
         }
+
         scope.pageSize = util.isMobile ? 99999 : Math.floor(winWid / tnWid);
 
         if (scope.expanded) {
           scope.pageSize *= 5;
         }
+
         var listLength = list ? list.length : 2;
+
         $timeout(function () {
           scope.pageCount = Math.ceil(listLength / scope.pageSize);
           spliceOnePage(scope);
