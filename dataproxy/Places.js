335fe1c39c3ec629cabc4a46dda10201f5b2bc17
Peter Williams <peter@newton.cx>
2020-03-22 12:03:35 -0400

    dataproxy/Places.js: restructure URL-munging code.
    This code does/did a bunch of work to munge URLs. All of that should be
    centralized through URLHelpers, so restructure to use that rather than custom
    stuff.
    
    Also remove addVampFeeds(), which was all commented out.
    
    Also set TileImage.aspx as a core dynamic URL.

--- dataproxy/Places.js
+++ dataproxy/Places.js
@@ -20,36 +20,31 @@ wwt.app.factory(
           rootFolders,
           openCollectionsFolder;
 
+      function rewrite_urls(item) {
+        if (item.get_thumbnailUrl) {
+          item.thumb = wwtlib.URLHelpers.singleton.rewrite(item.get_thumbnailUrl());
+        }
+
+        if (item.get_url) {
+          item.url = wwtlib.URLHelpers.singleton.rewrite(item.get_url());
+        }
+      }
+
       function getRoot() {
         var deferred = $q.defer();
+
         initPromise.then(function (folders) {
           rootFolders = folders;
           $.each(folders, function (i, item) {
             item.guid = item.get_name();
-            if (item.get_thumbnailUrl) {
-              fixThumb(item);
-            }
+            rewrite_urls(item);
           });
+
           transformData(folders);
           deferred.resolve(root.get_children());
         });
+
         return deferred.promise;
-      }
-
-      var cleanseUrl = function (fieldName, item) {
-        if (item[fieldName])
-          item[fieldName] = item[fieldName].replace("http://", "//");
-      }
-
-      var fixThumb = function (item) {
-        item.thumb = item.get_thumbnailUrl();
-
-        Object.keys(item).forEach(function (key) {
-          var lk = key.toLowerCase();
-          if (lk.indexOf('url') > -1 || lk.indexOf('thumb') > -1) {
-            cleanseUrl(key, item);
-          }
-        });
       }
 
       function getChildren(obj) {
@@ -57,12 +52,12 @@ wwt.app.factory(
 
         obj.childLoadCallback(function () {
           var children = obj.get_children();
+
           $.each(children, function (i, item) {
             item.guid = obj.guid + '.' + (item.get_isFolder() ? item.get_name() : i);
-            if (item.get_thumbnailUrl) {
-              fixThumb(item);
-            }
+            rewrite_urls(item);
           });
+
           deferred.resolve(transformData(children));
         });
 
@@ -80,30 +75,23 @@ wwt.app.factory(
       };
 
       var transformData = function (items) {
-        //console.warn({items:items});
         $.each(items, function (i, item) {
-          try {
-            if (typeof item.get_type == 'function') {
-              item.isPlanet = item.get_type() === 1;
-              //item.isFolder = item.get_type() === 0;
-              item.isFGImage = item.get_type() === 2 && typeof item.get_camParams == 'function';
-            }
-            if (typeof item.get_dataSetType == 'function') {
-              item.isEarth = item.get_dataSetType() === 0;
-              item.isPanorama = item.get_dataSetType() === 3;
-              item.isSurvey = item.get_dataSetType() === 2;
-              item.isPlanet = item.get_dataSetType() === 1;
-            }
-            Object.keys(item).forEach(function (key) {
-              var lk = key.toLowerCase();
-              if (lk.indexOf('url') > -1 || lk.indexOf('thumb') > -1) {
-                cleanseUrl(key, item);
-              }
-            });
-          } catch (er) {
-            util.log(item, er);
+          if (typeof item.get_type == 'function') {
+            item.isPlanet = item.get_type() === 1;
+            //item.isFolder = item.get_type() === 0;
+            item.isFGImage = item.get_type() === 2 && typeof item.get_camParams == 'function';
           }
+
+          if (typeof item.get_dataSetType == 'function') {
+            item.isEarth = item.get_dataSetType() === 0;
+            item.isPanorama = item.get_dataSetType() === 3;
+            item.isSurvey = item.get_dataSetType() === 2;
+            item.isPlanet = item.get_dataSetType() === 1;
+          }
+
+          rewrite_urls(item);
         });
+
         return items;
       };
 
@@ -122,6 +110,7 @@ wwt.app.factory(
 
           root.loadFromUrl(url, function () {
             var collection;
+
             if (util.getQSParam('wtml') != null) {
               openCollectionsFolder = wwt.wc.createFolder();
               openCollectionsFolder.set_name('Open Collections');
@@ -130,17 +119,13 @@ wwt.app.factory(
                 collection.get_children();
                 openCollectionsFolder.addChildFolder(collection);
                 root.addChildFolder(openCollectionsFolder);
-                //addVampFeeds();
                 deferred.resolve(root.get_children());
               });
             } else if (location.href.indexOf('?image=') !== -1) {
-              //addVampFeeds();
               importImage(location.href.split('?image=')[1]).then(function (data) {
                 deferred.resolve(root.get_children());
               });
-
             } else {
-              //addVampFeeds();
               deferred.resolve(root.get_children());
             }
           });
@@ -152,46 +137,41 @@ wwt.app.factory(
       };
 
       function openCollection(url) {
-
         var deferred = $q.defer();
+
         if (!openCollectionsFolder) {
           openCollectionsFolder = wwt.wc.createFolder();
           openCollectionsFolder.set_name('Open Collections');
           openCollectionsFolder.guid = 'f0';
           root.addChildFolder(openCollectionsFolder);
         }
+
         var collection = wwt.wc.createFolder();
+
         collection.loadFromUrl(url, function () {
-          //collection.get_children();
           collection.url = url;
           openCollectionsFolder.addChildFolder(collection);
+
           if (collection.get_name() == '') {
             deferred.resolve(collection.get_children());
           } else {
             deferred.resolve(collection);
           }
         });
+
         return deferred.promise;
-      }
-
-      function addVampFeeds() {
-        var vampFolder = wwt.wc.createFolder();
-        vampFolder.set_name('New Imagery');
-        vampFolder.guid = '0v0';
-        vampFolder.set_url('//worldwidetelescope.org/wwtweb/catalog.aspx?W=vampfeeds');
-        root.addChildFolder(vampFolder);
-
       }
 
       function importImage(url, manualData) {
         var deferred = $q.defer();
+
         if (!openCollectionsFolder) {
           openCollectionsFolder = wwt.wc.createFolder();
           openCollectionsFolder.set_name('Open Collections');
           root.addChildFolder(openCollectionsFolder);
         }
-        var collection = wwt.wc.createFolder();
 
+        var collection = wwt.wc.createFolder();
         collection.set_name("Imported image");
         //collection.url = url;
 
@@ -199,11 +179,13 @@ wwt.app.factory(
         if (manualData) {
           encodedUrl += manualData;
         }
-        collection.loadFromUrl('//worldwidetelescope.org/WWTWeb/TileImage.aspx?imageurl=' + encodedUrl, function () {
-          //collection.get_children();
-          //collection.url = url;
+
+        var url = wwtlib.URLHelpers.singleton.coreDynamicUrl('/wwtweb/TileImage.aspx?imageurl=' + encodedUrl);
+
+        collection.loadFromUrl(url, function () {
           if (collection.get_children()[0].get_RA() != 0 || collection.get_children()[0].get_dec() != 0) {
             openCollectionsFolder.addChildFolder(collection);
+
             getChildren(collection).then(function (children) {
               if (collection.get_name() == '') {
                 deferred.resolve(collection.get_children());
