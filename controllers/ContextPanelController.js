4362135bf544b3ebb963ad4c34eebadb3582e6f5
Peter Williams <peter@newton.cx>
2020-03-22 12:41:47 -0400

    controllers/ContextPanelController.js: reformat

--- controllers/ContextPanelController.js
+++ controllers/ContextPanelController.js
@@ -1,89 +1,93 @@
-﻿wwt.controllers.controller('ContextPanelController',
-	['$scope',
-	'$rootScope',
-	'$timeout', 
-	'Util', 
-	'SearchUtil',
-	'ThumbList',
-	function ($scope, $rootScope, $timeout, util, searchUtil, thumbList) {
-	    
-	    var lastUpdate = new Date();
+﻿wwt.controllers.controller(
+  'ContextPanelController',
+  [
+    '$scope',
+    '$rootScope',
+    '$timeout',
+    'Util',
+    'SearchUtil',
+    'ThumbList',
 
-	    var init = function () {
-	        $scope.isContextPanel = true;
-	        thumbList.init($scope, 'context');
-            
-	        $scope.placesInCone = [];
-	        $scope.scrollDepth = 40;
-	        $rootScope.$on('viewportchange', function (event, viewport) {
-	            if ((!viewport.isDirty && !viewport.init) || new Date().valueOf() - lastUpdate.valueOf() > 2000) {
-	                findNearbyObjects();
-	                lastUpdate = new Date();
-	            }
-	        });
-	        $scope.$watch('lookAt', findNearbyObjects);
-	    };
+    function ($scope, $rootScope, $timeout, util, searchUtil, thumbList) {
+      var lastUpdate = new Date();
 
-	    $scope.clickThumb = function (item,instant) {
+      var init = function () {
+	$scope.isContextPanel = true;
+	thumbList.init($scope, 'context');
+
+	$scope.placesInCone = [];
+	$scope.scrollDepth = 40;
+	$rootScope.$on('viewportchange', function (event, viewport) {
+	  if ((!viewport.isDirty && !viewport.init) || new Date().valueOf() - lastUpdate.valueOf() > 2000) {
+	    findNearbyObjects();
+	    lastUpdate = new Date();
+	  }
+	});
+	$scope.$watch('lookAt', findNearbyObjects);
+      };
+
+      $scope.clickThumb = function (item,instant) {
         $rootScope.instant = !!instant;
-	      thumbList.clickThumb(item, $scope);
-	      util.removeHoverCircle();
-	    };
+	thumbList.clickThumb(item, $scope);
+	util.removeHoverCircle();
+      };
 
-	    $scope.hoverThumb = function (item) {
-	        $scope.drawCircleOverPlace(item);
-	    };
+      $scope.hoverThumb = function (item) {
+	$scope.drawCircleOverPlace(item);
+      };
 
-	    var calcPageSize = function () {
-	        thumbList.calcPageSize($scope, true);
-	    };
+      var calcPageSize = function () {
+	thumbList.calcPageSize($scope, true);
+      };
 
-	    $(window).on('resize', function () {
-	        $scope.currentPage = 0;
-	        calcPageSize();
-	        
-	    });
+      $(window).on('resize', function () {
+	$scope.currentPage = 0;
+	calcPageSize();
+      });
 
-	    $scope.moveNboMenu = function (i) {
-	        $('#nboMenuContainer' + i).append($('#researchMenu'));
-	    };
-	    $scope.showMenu = function (item, i) {
-	        $('.popover-content .close-btn').click();
-	        setTimeout(function () {
-	            $('.popover-content .close-btn').click();
-	            $('#menuContainer' + i).find('.dropdown').addClass('open');
-	        }, 10);
-	        $scope.setMenuContextItem(item, true);
-	    };
-	    $scope.preventClickBubble = function (event) {
-	        event.stopImmediatePropagation();
-	    };
+      $scope.moveNboMenu = function (i) {
+	$('#nboMenuContainer' + i).append($('#researchMenu'));
+      };
 
-	    $scope.goBack = function () {
-	        thumbList.goBack($scope);
-	    };
-	    $scope.goFwd = function () {
-	        thumbList.goFwd($scope);
-	    };
+      $scope.showMenu = function (item, i) {
+	$('.popover-content .close-btn').click();
+	setTimeout(function () {
+	  $('.popover-content .close-btn').click();
+	  $('#menuContainer' + i).find('.dropdown').addClass('open');
+	}, 10);
+	$scope.setMenuContextItem(item, true);
+      };
 
-	    function findNearbyObjects() {
-	        if (($scope.lookAt === 'Sky' || $scope.lookAt === 'SolarSystem') && !wwt.tourPlaying) {
-	            searchUtil.findNearbyObjects({
-	                lookAt: $scope.lookAt,
-	                singleton: $rootScope.singleton
-	            }).then(function (result) {
-	                $scope.currentPage = 0;
-	                $('body').append($('#researchMenu'));
-	                $scope.collection = result;
-	                if (util.isMobile) {
-	                    $scope.setNBO($scope.collection);
-	                }
-	                calcPageSize($scope.collection);
-	            });
-	        }
+      $scope.preventClickBubble = function (event) {
+	event.stopImmediatePropagation();
+      };
+
+      $scope.goBack = function () {
+	thumbList.goBack($scope);
+      };
+
+      $scope.goFwd = function () {
+	thumbList.goFwd($scope);
+      };
+
+      function findNearbyObjects() {
+	if (($scope.lookAt === 'Sky' || $scope.lookAt === 'SolarSystem') && !wwt.tourPlaying) {
+	  searchUtil.findNearbyObjects({
+	    lookAt: $scope.lookAt,
+	    singleton: $rootScope.singleton
+	  }).then(function (result) {
+	    $scope.currentPage = 0;
+	    $('body').append($('#researchMenu'));
+	    $scope.collection = result;
+	    if (util.isMobile) {
+	      $scope.setNBO($scope.collection);
 	    }
+	    calcPageSize($scope.collection);
+	  });
+	}
+      }
 
-	    init();
-
-	}]);
-
+      init();
+    }
+  ]
+);
