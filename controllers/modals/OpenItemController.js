4946a0a43c231b41d9ae761a91a8e6b788bae8da
Peter Williams <peter@newton.cx>
2020-04-06 14:32:26 -0400

    controllers/modals/OpenItemController.js: reindent and kill commented-out stuff

--- controllers/modals/OpenItemController.js
+++ controllers/modals/OpenItemController.js
@@ -1,22 +1,27 @@
-﻿wwt.controllers.controller('OpenItemController',
-  ['$rootScope',
+﻿wwt.controllers.controller(
+  'OpenItemController',
+  [
+    '$rootScope',
     '$scope',
     'AppState',
     'Places',
     'Util',
-    'Astrometry', 'MediaFile',
-    function ($rootScope, $scope, appState, places, util, astrometry, media) {
+    'Astrometry',
+    'MediaFile',
 
+    function ($rootScope, $scope, appState, places, util, astrometry, media) {
       $rootScope.$on('openItem', function () {
         $scope.openItemUrl = '';
         setTimeout(function () {
           $('#txtOpenItem').focus();
         }, 100);
       });
-      $scope.tour={edit:false};
+
+      $scope.tour = {edit: false};
 
       $scope.openItem = function () {
         var itemType = $rootScope.openType;
+
         if (itemType === 'collection') {
           places.openCollection($scope.openItemUrl).then(function (folder) {
             util.log('initExplorer broadcast', folder);
@@ -25,7 +30,7 @@
             $('#openModal').modal('hide');
           });
         } else if (itemType === 'tour') {
-         // console.log({editTour:$scope.tour.edit});
+          // console.log({editTour:$scope.tour.edit});
           $scope.playTour($scope.openItemUrl, !!$scope.tour.edit);
           $('#openModal').modal('hide');
         } else if (itemType === 'FITS image') {
@@ -33,10 +38,7 @@
           setTimeout(wwt.detectNewLayers, 555);
           $('#openModal').modal('hide');
         } else {
-          //var qs = '&ra=202.45355674088898&dec=47.20018130592933&scale=' + (0.3413275776344843 / 3600) + '&rotation=122.97953942448784';
-          //$scope.openItemUrl = '//www.noao.edu/outreach/aop/observers/m51rolfe.jpg';
           places.importImage($scope.openItemUrl).then(function (folder) {
-            //var imported = folder.get_children()[0];
             if (folder) {
               util.log('initExplorer broadcast', folder);
               $rootScope.newFolder = folder;
@@ -54,6 +56,7 @@
         var type = $rootScope.openType;
         console.time('openLocal: ' + type);
         var file = e.target.files[0];
+
         if (!file.name) {
           return;
         }
@@ -69,8 +72,8 @@
       };
 
       $scope.astrometryStatusText = '';
-      $scope.astroCallback = function (data) {
 
+      $scope.astroCallback = function (data) {
         $scope.$applyAsync(function () {
           if ($scope.astrometryStatusText.indexOf(data.message) == 0) {
             $scope.astrometryStatusText += ' .';
@@ -78,24 +81,18 @@
             $scope.astrometryStatusText = data.message;
           }
         });
+
         if (data.calibration) {
-          /*calibration.ra = result.ra; // in degrees devide 15 for hours
-          calibration.dec = result.dec; // in degrees
-          calibration.rotation = result.orientation;
-          calibration.scale = result.pixscale;
-          calibration.parity = result.parity;
-          calibration.radius = result.radius;*/
           $scope.importState = 'astrometrySuccess';
           var qs = '&ra=' + data.calibration.ra +
-            '&dec=' + data.calibration.dec +
-            '&scale=' + (data.calibration.scale / 3600) +
-            '&rotation=' + data.calibration.rotation;
+              '&dec=' + data.calibration.dec +
+              '&scale=' + (data.calibration.scale / 3600) +
+              '&rotation=' + data.calibration.rotation;
           if (data.calibration.parity !== 1) {
             qs += '&reverseparity=true';
           }
 
           places.importImage($scope.openItemUrl, qs).then(function (folder) {
-
             $rootScope.newFolder = folder;
             $rootScope.$broadcast('initExplorer', $scope.openItemUrl);
             $scope.imageFail = false;
@@ -104,9 +101,9 @@
             return;
           });
         }
+
         if (data.status.toLowerCase().indexOf('fail') != -1) {
           $scope.importState = 'astrometryFail';
-
         }
       }
 
@@ -114,7 +111,6 @@
         $scope.importState = 'astrometryProgress';
         astrometry.submitImage($scope.openItemUrl, $scope.astroCallback, false);
       };
-
     }
-  ]);
-
+  ]
+);
