26c331f618c1ce50659f4723f0fad63dcade4b77
Peter Williams <peter@newton.cx>
2020-10-27 11:50:48 -0400

    factories/AppState.js: hide the Layer Manager by default
    Also rework how the app state is initialized to try to be a bit more
    robust.

--- factories/AppState.js
+++ factories/AppState.js
@@ -34,9 +34,25 @@ wwt.app.factory(
       return data;
     }
 
+    var initialState = {
+      layerManagerHidden: true,
+    };
+
     var init = function () {
-      var storedData = localStorage ? localStorage.getItem('appState') : {};
-      data = storedData && localStorage ? JSON.parse(storedData) : {};
+      try {
+        var storedString = localStorage.getItem('appState');
+        var storedObj = JSON.parse(storedString);
+
+        // This seems like the most robust way to make sure that we got a
+        // dictlike (N.B., JSON.parse(null) => null).
+        if (Object.prototype.toString.call(storedObj) != '[object Object]') {
+          throw new Error('stored object not dict');
+        }
+
+        data = storedObj;
+      } catch {
+        data = initialState;
+      }
     };
 
     init();
